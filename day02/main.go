c03522e927c2672ce46e5b9856fdbf91ee2b4453
Felix Breidenstein <mail@felixbreidenstein.de>
2019-12-02 23:34:35 +0100

    reformating of day2

--- day02/main.go
+++ day02/main.go
@@ -8,14 +8,17 @@ import (
 	"strings"
 )
 
-func execute(progString string) string {
+func format(progString string) []int {
 	var prog []int
-	pos := 0
 	for _, s := range strings.Split(progString, ",") {
 		cmd, _ := strconv.Atoi(s)
 		prog = append(prog, cmd)
 	}
+	return prog
+}
 
+func execute(prog []int) []int {
+	pos := 0
 	for {
 		cmd := prog[pos]
 		switch cmd {
@@ -32,7 +35,7 @@ func execute(progString string) string {
 			prog[target] = prog[left] * prog[right]
 			pos += 4
 		case 99:
-			return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(prog)), ","), "[]")
+			return prog
 		}
 	}
 
@@ -42,12 +45,15 @@ func main() {
 	f, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(f)
 
-	var programm string
+	var progString string
 
 	for scanner.Scan() {
-		programm = scanner.Text()
+		progString = scanner.Text()
 	}
+	prog := format(progString)
+	prog[1] = 12
+	prog[2] = 2
 
-	fmt.Printf("Result is %q\n", execute(programm))
-
+	result := execute(prog)
+	fmt.Printf("Result is %d\n", result[0])
 }
