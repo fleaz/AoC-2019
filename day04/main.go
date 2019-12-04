5a4cbbce158be03a38e434bd19ffa04693426928
Felix Breidenstein <mail@felixbreidenstein.de>
2019-12-04 22:18:33 +0100

    Added day04 part 2

--- day04/main.go
+++ day04/main.go
@@ -5,7 +5,31 @@ import (
 	"strconv"
 )
 
-func isValid(password int) bool {
+func checkRealPair(pos int, value byte, pwString string) bool {
+	var left, right byte
+
+	if pos+2 == len(pwString) {
+		// we are at the right bound of pwString
+		left = pwString[pos-1]
+		right = 'x'
+	} else if pos == 0 {
+		// we are at the left bound of pwString
+		left = 'x'
+		right = pwString[pos+2]
+	} else {
+		// we are in the middle
+		left = pwString[pos-1]
+		right = pwString[pos+2]
+	}
+
+	if (left == value) || (right == value) {
+		return false
+	}
+
+	return true
+}
+
+func isValid(password int, extendet bool) bool {
 	pwString := strconv.Itoa(password)
 
 	//Length criteria
@@ -14,16 +38,19 @@ func isValid(password int) bool {
 	}
 
 	// adjavent digits criteria
-	pair := false
+	pair := 0
+	realPair := 0
 	for i := 0; i < len(pwString)-1; i++ {
 		left := pwString[i]
 		right := pwString[i+1]
 		if left == right {
-			pair = true
-			break
+			pair += 1
+			if checkRealPair(i, left, pwString) {
+				realPair += 1
+			}
 		}
 	}
-	if pair == false {
+	if pair == 0 {
 		return false
 	}
 
@@ -36,14 +63,19 @@ func isValid(password int) bool {
 		}
 	}
 
+	// check extra rule for part 2
+	if extendet && realPair == 0 {
+		return false
+	}
+
 	// All check passed
 	return true
 }
 
-func findValidPasswords(from int, to int) int {
+func findValidPasswords(from int, to int, extendet bool) int {
 	counter := 0
 	for i := from; i <= to; i++ {
-		if isValid(i) {
+		if isValid(i, extendet) {
 			counter++
 		}
 	}
@@ -55,6 +87,9 @@ func main() {
 	from := 359282
 	to := 820401
 
-	count := findValidPasswords(from, to)
+	count := findValidPasswords(from, to, false)
 	fmt.Printf("Result for part 1 is %d\n", count)
+
+	count2 := findValidPasswords(from, to, true)
+	fmt.Printf("Result for part 2 is %d\n", count2)
 }
--- day04/main_test.go
+++ day04/main_test.go
@@ -7,16 +7,33 @@ type testpair struct {
 	want  bool
 }
 
-func Test(t *testing.T) {
+// func Test(t *testing.T) {
+// 	pairs := []testpair{
+// 		{111111, true},
+// 		{223450, false},
+// 		{123789, false},
+// 		{777770, false},
+// 	}
+//
+// 	for _, p := range pairs {
+// 		is := isValid(p.value, false)
+// 		if is != p.want {
+// 			t.Errorf("%d: Wanted %v but got %v", p.value, p.want, is)
+// 		}
+//
+// 	}
+// }
+
+func TestExtendet(t *testing.T) {
 	pairs := []testpair{
-		{111111, true},
-		{223450, false},
-		{123789, false},
-		{777770, false},
+		// {112233, true},
+		// {123444, false},
+		// {111122, true},
+		{667789, true},
 	}
 
 	for _, p := range pairs {
-		is := isValid(p.value)
+		is := isValid(p.value, true)
 		if is != p.want {
 			t.Errorf("%d: Wanted %v but got %v", p.value, p.want, is)
 		}
